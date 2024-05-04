0b9dbb7968ba627539cf28df5f3511b812aeeed6
Hackfront <hackfront@protonmail.ch>
2024-05-04 18:03:39 +0200

    Add project base

---
+++ main/config.h
@@ -0,0 +1,9 @@
+#ifndef A_CONFIG_H
+#define A_CONFIG_H
+
+#define NET_WIFI_SSID       "SSID"
+#define NET_WIFI_PASSWORD   "Password"
+
+#include "secrets.h"
+
+#endif /* end of include guard: A_CONFIG_H */
