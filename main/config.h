c671d27fb12d0e99c6a7ce2fc6fa70bc99b2e3b0
Hackfront <hackfront@protonmail.ch>
2024-05-18 12:11:21 +0200

    Add MQTT server config

--- main/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef A_CONFIG_H
-#define A_CONFIG_H
-
-#define NET_WIFI_SSID       "Casa Rock"
-#define NET_WIFI_PASSWORD   "<password>"
-
-#define MQTT_BROKER_URL  "mqtts://tesla.casarock.local:8883"
-
-#endif /* end of include guard: A_CONFIG_H */
