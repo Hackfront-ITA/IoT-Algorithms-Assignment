9b6550e4d48702c8c370fe604c8825c14f12a134
Hackfront <hackfront@protonmail.ch>
2024-05-20 11:13:21 +0200

    Fix indentation style

--- main/adc.c
+++ main/adc.c
@@ -24,13 +24,13 @@
 #define A_ADC_READ_LEN       1024
 
 #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-  #define A_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
-  #define A_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
-  #define A_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+	#define A_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
+	#define A_ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
+	#define A_ADC_GET_DATA(p_data)        ((p_data)->type1.data)
 #else
-  #define A_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
-  #define A_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
-  #define A_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
+	#define A_ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
+	#define A_ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
+	#define A_ADC_GET_DATA(p_data)        ((p_data)->type2.data)
 #endif
 
 static const char *TAG = "ADC module";
@@ -41,91 +41,91 @@ static uint8_t a_result[A_ADC_READ_LEN];
 static adc_continuous_handle_t a_handle = NULL;
 
 static adc_digi_pattern_config_t a_dpc = {
-  .unit = A_ADC_UNIT,
-  .channel = A_ADC_CHANNEL & 0x07,
-  .atten = A_ADC_ATTEN,
-  .bit_width = A_ADC_BIT_WIDTH,
+	.unit = A_ADC_UNIT,
+	.channel = A_ADC_CHANNEL & 0x07,
+	.atten = A_ADC_ATTEN,
+	.bit_width = A_ADC_BIT_WIDTH,
 };
 
 static adc_continuous_handle_cfg_t a_handle_cfg = {
-  .max_store_buf_size = A_ADC_BUF_SIZE,
-  .conv_frame_size = A_ADC_READ_LEN,
+	.max_store_buf_size = A_ADC_BUF_SIZE,
+	.conv_frame_size = A_ADC_READ_LEN,
 };
 
 static adc_continuous_config_t a_config = {
-  .sample_freq_hz = A_ADC_SAMPLING_FREQ,
-  .conv_mode = A_ADC_CONV_MODE,
-  .format = A_ADC_OUTPUT_TYPE,
-  .pattern_num = 1,
-  .adc_pattern = &a_dpc,
+	.sample_freq_hz = A_ADC_SAMPLING_FREQ,
+	.conv_mode = A_ADC_CONV_MODE,
+	.format = A_ADC_OUTPUT_TYPE,
+	.pattern_num = 1,
+	.adc_pattern = &a_dpc,
 };
 
 static bool IRAM_ATTR a_conv_done_cb(adc_continuous_handle_t handle,
-    const adc_continuous_evt_data_t *edata, void *user_data)
+		const adc_continuous_evt_data_t *edata, void *user_data)
 {
-  return true;
+	return true;
 }
 
 static bool IRAM_ATTR a_pool_ovf_cb(adc_continuous_handle_t handle,
-    const adc_continuous_evt_data_t *edata, void *user_data)
+		const adc_continuous_evt_data_t *edata, void *user_data)
 {
 	overflow_count++;
 
-  return true;
+	return true;
 }
 
 static adc_continuous_evt_cbs_t a_evt_cbs = {
-  .on_conv_done = a_conv_done_cb,
-  .on_pool_ovf = a_pool_ovf_cb,
+	.on_conv_done = a_conv_done_cb,
+	.on_pool_ovf = a_pool_ovf_cb,
 };
 
 esp_err_t a_adc_init(void) {
-  esp_err_t err;
+	esp_err_t err;
 
-  err = adc_continuous_new_handle(&a_handle_cfg, &a_handle);
-  if (err != ESP_OK) {
-    return err;
-  }
+	err = adc_continuous_new_handle(&a_handle_cfg, &a_handle);
+	if (err != ESP_OK) {
+		return err;
+	}
 
-  err = adc_continuous_register_event_callbacks(a_handle, &a_evt_cbs, NULL);
-  if (err != ESP_OK) {
-    return err;
-  }
+	err = adc_continuous_register_event_callbacks(a_handle, &a_evt_cbs, NULL);
+	if (err != ESP_OK) {
+		return err;
+	}
 
-  err = adc_continuous_config(a_handle, &a_config);
-  if (err != ESP_OK) {
-    return err;
-  }
+	err = adc_continuous_config(a_handle, &a_config);
+	if (err != ESP_OK) {
+		return err;
+	}
 
-  return ESP_OK;
+	return ESP_OK;
 }
 
 esp_err_t a_adc_set_sampling_freq(uint32_t sampling_freq) {
-  esp_err_t err;
+	esp_err_t err;
 
-  a_config.sample_freq_hz = sampling_freq;
+	a_config.sample_freq_hz = sampling_freq;
 
-  err = adc_continuous_config(a_handle, &a_config);
-  if (err != ESP_OK) {
-    return err;
-  }
+	err = adc_continuous_config(a_handle, &a_config);
+	if (err != ESP_OK) {
+		return err;
+	}
 
-  return ESP_OK;
+	return ESP_OK;
 }
 
 esp_err_t a_adc_start(void) {
-  return adc_continuous_start(a_handle);
+	return adc_continuous_start(a_handle);
 }
 
 esp_err_t a_adc_stop(void) {
-  return adc_continuous_stop(a_handle);
+	return adc_continuous_stop(a_handle);
 }
 
 esp_err_t a_adc_collect_samples(float *buffer, size_t length, float flush_freq) {
-  size_t index = 0;
-  uint16_t waiting_time = floor((A_ADC_READ_LEN / 4) * 1000.0 / flush_freq);
-  waiting_time = C_MAX(waiting_time, 1);
-  ESP_LOGI(TAG, "waiting_time = %hu", waiting_time);
+	size_t index = 0;
+	uint16_t waiting_time = floor((A_ADC_READ_LEN / 4) * 1000.0 / flush_freq);
+	waiting_time = C_MAX(waiting_time, 1);
+	ESP_LOGI(TAG, "waiting_time = %hu", waiting_time);
 
 	while (index < length) {
 		vTaskDelay(C_DELAY_MS(waiting_time));
@@ -133,13 +133,13 @@ esp_err_t a_adc_collect_samples(float *buffer, size_t length, float flush_freq) 
 		uint32_t rsize = 0;
 
 		esp_err_t err = adc_continuous_read(a_handle, a_result, A_ADC_READ_LEN,
-      &rsize, A_ADC_READ_TIMEOUT);
+			&rsize, A_ADC_READ_TIMEOUT);
 
 		if (err == ESP_ERR_TIMEOUT) {
-      ESP_LOGE(TAG, "ADC read timeout");
+			ESP_LOGE(TAG, "ADC read timeout");
 			return err;
 		} else if (err != ESP_OK) {
-      ESP_LOGE(TAG, "Other ADC error: %d", err);
+			ESP_LOGE(TAG, "Other ADC error: %d", err);
 			return err;
 		}
 
@@ -157,7 +157,7 @@ esp_err_t a_adc_collect_samples(float *buffer, size_t length, float flush_freq) 
 				index++;
 			} else {
 				ESP_LOGW(TAG, "Invalid data [ADC_UNIT_?, %" PRIu16 "_%" PRIx16 "]",
-          channel, data);
+					channel, data);
 			}
 		}
 	}
@@ -165,5 +165,5 @@ esp_err_t a_adc_collect_samples(float *buffer, size_t length, float flush_freq) 
 	ESP_LOGI(TAG, "overflow_count = %lu", overflow_count);
 	overflow_count = 0;
 
-  return ESP_OK;
+	return ESP_OK;
 }
--- main/mqtt.c
+++ main/mqtt.c
@@ -12,14 +12,14 @@ extern const uint8_t a_mqtt_cert[]   asm("_binary_mqtt_cert_pem_start");
 static const char *TAG = "MQTT";
 
 static esp_mqtt_client_config_t a_mqtt_cfg = {
-  .broker = {
-    .address.uri = MQTT_BROKER_URL,
-    .verification = {
+	.broker = {
+		.address.uri = MQTT_BROKER_URL,
+		.verification = {
 			.certificate = (const char *)(a_mqtt_cert),
 			.skip_cert_common_name_check = true,
 			// .common_name = "MQTT Server",
 		}
-  }
+	}
 };
 
 static esp_mqtt_client_handle_t a_client;
@@ -29,64 +29,64 @@ static void a_mqtt_on_disconnect(void *handler_args, esp_event_base_t base, int3
 static void a_mqtt_on_error(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 esp_err_t a_mqtt_start(void) {
-  a_client = esp_mqtt_client_init(&a_mqtt_cfg);
-  if (a_client == NULL) {
-    return ESP_FAIL;
-  }
-  esp_mqtt_client_register_event(a_client, MQTT_EVENT_CONNECTED,    a_mqtt_on_connect,     NULL);
-  esp_mqtt_client_register_event(a_client, MQTT_EVENT_DISCONNECTED, a_mqtt_on_disconnect,  NULL);
-  esp_mqtt_client_register_event(a_client, MQTT_EVENT_ERROR,        a_mqtt_on_error,       NULL);
-  esp_mqtt_client_start(a_client);
+	a_client = esp_mqtt_client_init(&a_mqtt_cfg);
+	if (a_client == NULL) {
+		return ESP_FAIL;
+	}
+	esp_mqtt_client_register_event(a_client, MQTT_EVENT_CONNECTED,    a_mqtt_on_connect,     NULL);
+	esp_mqtt_client_register_event(a_client, MQTT_EVENT_DISCONNECTED, a_mqtt_on_disconnect,  NULL);
+	esp_mqtt_client_register_event(a_client, MQTT_EVENT_ERROR,        a_mqtt_on_error,       NULL);
+	esp_mqtt_client_start(a_client);
 
-  return ESP_OK;
+	return ESP_OK;
 }
 
 esp_err_t a_mqtt_stop(void) {
-  return ESP_OK;
+	return ESP_OK;
 }
 
 esp_err_t a_mqtt_publish(const char *topic, const char *data) {
-  int msg_id;
+	int msg_id;
 
-  msg_id = esp_mqtt_client_enqueue(a_client, topic, data, 0, 0, 0, true);
+	msg_id = esp_mqtt_client_enqueue(a_client, topic, data, 0, 0, 0, true);
 
-  if (msg_id < 0) {
-    ESP_LOGE(TAG, "Error publishing message, rc=%d", msg_id);
-    return ESP_FAIL;
-  }
+	if (msg_id < 0) {
+		ESP_LOGE(TAG, "Error publishing message, rc=%d", msg_id);
+		return ESP_FAIL;
+	}
 
-  return ESP_OK;
+	return ESP_OK;
 }
 
 
 static void a_mqtt_on_connect(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
-  ESP_LOGI(TAG, "Connected");
+	ESP_LOGI(TAG, "Connected");
 
 	a_mqtt_connected = true;
 
-  int msg_id = esp_mqtt_client_publish(a_client, "/tests/esp32/status", "ALIVE", 0, 0, true);
-  ESP_LOGI(TAG, "Sent alive message, msg_id=%d", msg_id);
+	int msg_id = esp_mqtt_client_publish(a_client, "/tests/esp32/status", "ALIVE", 0, 0, true);
+	ESP_LOGI(TAG, "Sent alive message, msg_id=%d", msg_id);
 };
 
 static void a_mqtt_on_disconnect(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
-  ESP_LOGI(TAG, "Disconnected");
+	ESP_LOGI(TAG, "Disconnected");
 
 	a_mqtt_connected = false;
 };
 
 static void a_mqtt_on_error(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
-  ESP_LOGI(TAG, "Generic error");
+	ESP_LOGI(TAG, "Generic error");
 
-  // if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-  //   ESP_LOGE(TAG, "Last error reported from esp-tls: 0x%x",
-  //       event->error_handle->esp_tls_last_esp_err);
-  //
-  //   ESP_LOGE(TAG, "Last error reported from tls stack: 0x%x",
-  //       event->error_handle->esp_tls_stack_err);
-  //
-  //   ESP_LOGE(TAG, "Last error captured as transport's socket errno: 0x%x",
-  //       event->error_handle->esp_transport_sock_errno);
-  //
-  //   ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-  // }
+	// if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+	//   ESP_LOGE(TAG, "Last error reported from esp-tls: 0x%x",
+	//       event->error_handle->esp_tls_last_esp_err);
+	//
+	//   ESP_LOGE(TAG, "Last error reported from tls stack: 0x%x",
+	//       event->error_handle->esp_tls_stack_err);
+	//
+	//   ESP_LOGE(TAG, "Last error captured as transport's socket errno: 0x%x",
+	//       event->error_handle->esp_transport_sock_errno);
+	//
+	//   ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+	// }
 };
